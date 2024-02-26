8346f6e5fe6be6448f8425619bbfeeeb96f4cee8
Ían Muliterno <iannimuliterno@gmail.com>
2024-02-25 22:02:42 -0300

    implemented portfolio optm funcion
    for a first version, the intention is to keep 2 fixed constrains, `full_investment` and `long_only` .the objectives are to optmize the return and the variation under the chosen risk

---
+++ logic/portfolioOptim.R
@@ -0,0 +1,25 @@
+function(dt,risk){
+  
+  return_df <- dt |> 
+    mutate(return = (close-open)/open) |> 
+    select(date = timestamp,name,return) |> 
+    tidyr::pivot_wider(names_from = name,values_from = return) |> 
+    janitor::clean_names()
+  
+  
+  return_df <- tbl2xts::tbl_xts(return_df)
+  
+  portfolio <- portfolio.spec(assets = colnames(dt))
+  portfolio <- add.constraint(portfolio, type = "full_investment")
+  portfolio <- add.constraint(portfolio, type = "long_only")
+  portfolio <- add.objective(portfolio, type = "return", name = "mean")
+  portfolio <- add.objective(portfolio, type = "risk", name = "StdDev", risk_aversion = risk)
+  optimal_weights <- optimize.portfolio(dt, portfolio)
+  
+  df_result <- data.frame(weights = as.numeric(optimal_weights$weights),
+             crypto = names(optimal_weights$portfolio$assets))
+  
+  return(df_result)
+  
+}
+
